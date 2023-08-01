f27c429f98161f736451e1fdb9503173ec473423
Romario Paixao <romagalera@gmail.com>
2023-07-31 23:27:47 -0300

    testando git

--- guanbara/python/listas.py
+++ guanbara/python/listas.py
@@ -8,10 +8,22 @@
 #     print(f'Na posição {c} encontrei o valor {v}!')
 # print('Cheguei ao final da lista.')
 
-valores = list()
-for cont in range (0, 5):
-    valores.append(int(input('Digite um valor: ')))
+# valores = list()
+# for cont in range (0, 5):
+#     valores.append(int(input('Digite um valor: ')))
+#
+# for c, v in enumerate(valores):
+#     print(f'Na posição {c} encontrei o valor {v}!')
+# print('Cheguei ao final da lista.')
+# #testando git
+frase ='Romário Paixão'
+print(frase.split())
+print('-'.join(frase))
+# print(frase.rstrip().lstrip())
+# print(f'o tamanho da frase é:',len(frase))
+# print(frase[9::3])
 
-for c, v in enumerate(valores):
-    print(f'Na posição {c} encontrei o valor {v}!')
-print('Cheguei ao final da lista.')
+
+# frase = 'Curso em Video Python'
+# print(len(frase))
+# print(frase[:21]
