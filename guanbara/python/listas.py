0e9b57642c5a36fb8511891dc1c07b6a209e3324
romariopaixao <romagalera@gmail.com>
2023-08-03 11:31:13 -0300

    Revert "Merge branch 'main' of https://github.com/romariopaixao/estudos"
    This reverts commit 8ac042000974b82cd596367fb44e38bc25473cda, reversing
    changes made to bc9d9b1a420494a6b26fbd7f08b9e6d7df5c2e70.

---
+++ guanbara/python/listas.py
@@ -0,0 +1,26 @@
+# valores = []
+# valores.append(2)
+# valores.append(3)
+# valores.append(4)
+# valores.append(7)
+#
+# for c, v in enumerate(valores):
+#     print(f'Na posição {c} encontrei o valor {v}!')
+# print('Cheguei ao final da lista.')
+
+# valores = list()
+# for cont in range (0, 5):
+#     valores.append(int(input('Digite um valor: ')))
+#
+# for c, v in enumerate(valores):
+#     print(f'Na posição {c} encontrei o valor {v}!')
+# print('Cheguei ao final da lista.')
+# #testando git
+
+lista = list()
+for cont in range(0,5):
+    lista.append(int(input('Digite um valor: ')))
+lista.sort()
+
+print(f'O menor da lista é {lista[0]} e sua posição é 0')
+print(lista)
