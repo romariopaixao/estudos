546b4cf19851ce6b6fb0500f6a4480076f5dd419
Romario Paixao <romagalera@gmail.com>
2023-08-15 16:51:11 -0300

    Aula 3 sobre webscreapping

--- web screaping/requisicoes.py
+++ web screaping/requisicoes.py
@@ -1,12 +1,22 @@
 import requests
-# Jogando a requisição atravaes do metodo get para a variavel response
-response = requests.get('https://www.w3schools.com/python/python_strings.asp')
+from bs4 import BeautifulSoup
 
-# Statuso do código, vai retornar algum daqueles status HTTPS
-print('Status code ',response.status_code)
-print('↓↓ Header ↓↓')
-# Método para trazer o cabecario do site
-print(response.headers)
-#Método para trazer todo o conteudo html do site requistado
-print('\n ↓↓ Content ↓↓')
-print(response.content)
+response = requests.get('https://g1.globo.com')
+# Jogando a requisição atravaes do metodo get para a variavel response
+
+content = response.content
+# Variavel contente recebe o conteudo HTML do site
+
+site = BeautifulSoup(content, 'html.parser')
+#Site recebe o conteudo do site, e converte para objeto. Segundo parametro indicando que é HTML
+
+noticia = site.find('div', attrs={'class': 'feed-post-body'})
+#HTML da noticia - Buca no site, pela tag dic com o atributo do dicionario acima
+
+titulo = noticia.find('a', attrs={'class': 'feed-post-link'})
+#HTML da do titulo da noticia - Buca na noticia, pela tag 'a' com o atributo do dicionario acima
+
+subtitulo = noticia.find('a', attrs={'class': 'gui-color-primary gui-color-hover feed-post-body-title bstn-relatedtext'})
+print(subtitulo.prettify()) #atributo prettify formata o texto para o padrao html
+print('Titulo da noticia: ', titulo.text) #atributo text pega so oconteudo de dentro da tag
+
